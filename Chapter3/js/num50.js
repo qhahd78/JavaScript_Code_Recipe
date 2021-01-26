2ac3e1a80101682f6ae64e14cf8e7becb6d0c751
qhahd78 <umuj0708@likelion.org>
2021-01-26 22:25:32 +0900

    num 50~ 51

---
+++ Chapter3/js/num50.js
@@ -0,0 +1,7 @@
+const array1 =['사과', '귤'];
+array1.unshift('바나나'); // 바나나를 배열 맨 처음에 추가 
+console.log(array1); 
+
+const array2 = ['사과', '귤'];
+array2.push('바나나', '딸기'); // 바나나, 딸기를 배열 맨 마지막에 추가 
+console.log (array2);
---
+++ Chapter3/js/num51.js
@@ -0,0 +1,11 @@
+// 배열 요소 삭제 
+
+const array1 = ['사과', '귤', '바나나'];
+const shiftedValue = array1.shift(); //첫 위치 요소 삭제 
+console.log(shiftedValue); // 삭제된 요소 출ㄹ력 
+console.log(array1); // 삭제 후 배열 출력 
+
+const array2 = ['사과', '귤', '바나나'];
+const poppedValue = array2.pop(); // 마지막 위치 요소 삭제 
+console.log(poppedValue); // 삭제된 요소 출력 
+console.log(array2); // 삭제 후 배열 출력 
