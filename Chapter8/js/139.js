4cec1548ae05e397b0cbd2212aa92ae1949e0308
qhahd78 <umuj0708@likelion.org>
2021-07-07 19:47:36 +0900

    comment add

--- Chapter8/js/139.js
+++ Chapter8/js/139.js
@@ -1,5 +1,7 @@
+// box 클래스를 가진 요소를 가져와 boxList 에 저장(array 형태로)
 const boxList = document.getElementsByClassName('box');
 
+// for 문을 돌면서 array 에 있는 요소 하나씩 선택, 콘솔에 출력 
 for (let index =0; boxList.length; index++) {
     console.log(boxList)
     console.log(boxList[index])
